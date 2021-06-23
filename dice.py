bf210d82aa50877778b98298f0ffe405810e0c37
krook98 <86385366+krook98@users.noreply.github.com>
2021-06-24 00:57:47 +0200

    Update dice.py

--- dice.py
+++ dice.py
@@ -2,17 +2,20 @@ import random
 
 numbers = [1,2,3,4,5,6]
 
-#Loop will repeat the block where it asks for input until 'n' is pressed.
+# Loop will repeat the block where it asks for input until 'n' is pressed.
 while True:
     x = input("Would you like to roll the dice? Press 'y' for yes or 'n' for no.")
     if x == 'y':
-        #Start rolling
+        # Start rolling
         roll = random.randrange(numbers[0], numbers[5], 1)
         print(roll)
     elif x == 'n':
-        #Say goodbye and exit
+        # Say goodbye and exit
         print("Goodbye.")
         break
+    # If somebody enters something else than y or n
+    else:
+        print("Please press 'y' for yes or 'n' for no")
 
 
 
